f5b1958099cee9b5cb613f3cde890ca4d6a003d2
syedkaleem05 <sye020067@iust.ac.in>
2026-04-22 15:53:10 +0530

    C

--- reverseTriangle.c
+++ reverseTriangle.c
@@ -3,7 +3,7 @@
         int i, j, n, k;
         printf("Write No. of Rows: ");
         scanf("%d", &n);
-        for(i = 1; i<=n; i++){
+        for(i = n; i>= 1; i--){
             for(j = i; j<= n-1; j++){
                 printf(" ");
             }
